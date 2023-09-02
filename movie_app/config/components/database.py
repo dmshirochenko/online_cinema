7e873d6dbf65b9a8fe9f102691fe154746457a1c
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2023-09-02 18:45:42 +0200

    Adding docker container

--- movie_app/config/components/database.py
+++ movie_app/config/components/database.py
@@ -7,7 +7,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'HOST': os.environ.get('DB_HOST_NAME', '127.0.0.1'),
         'PORT': os.environ.get('DB_PORT', 5432),
         'OPTIONS': {
            'options': '-c search_path=public,content'
